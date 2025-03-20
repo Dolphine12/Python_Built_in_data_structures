c96cf358d804be7a97d4cd4d752c188c40a9c0cb
Dolphine12 <daisyachieng246@gmail.com>
2025-03-20 21:43:50 +0300

    Python BUilt in data Structures

---
+++ Python_structures.py
@@ -0,0 +1,24 @@
+# Question 1: Creating an empty list
+my_list = []
+
+# Question 2: Appending elements to my_list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#Question 3: Inserting Value
+my_list.insert(1,15)
+
+#Question 4: Extendning the list
+my_list.extend([50,60,70])
+
+#Question 5: Removing last item
+my_list.pop()
+
+#Question 6: Sorting list in ascending
+my_list.sort()
+
+#Question 7: indexing and printing values
+index30 = my_list.index(30)
+print("\nIndex of 30: ", index30)
